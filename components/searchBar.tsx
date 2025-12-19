29648a449db5d42802b5a4ec9d86d23672ac28d9
UmerFarooqJillani <umerfarooqjillani92@gmail.com>
2025-12-19 22:10:57 +0500

    fetch the Search query Data and build the Search screen

--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -1,12 +1,14 @@
 import { icons } from "@/constants/icons";
 import { Image, TextInput, View } from "react-native";
 
-interface props {
+interface Props {
     placeholder: string;
     onPress?: () => void;
+    value: string;
+    onChangeText: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: props) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-2">
             <Image source={icons.search} className="size-6"
@@ -14,8 +16,8 @@ const SearchBar = ({ placeholder, onPress }: props) => {
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=""
-                onChangeText={() => { }}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor="#a8b5db"
                 className="flex-1 ml-2 text-white "
             />

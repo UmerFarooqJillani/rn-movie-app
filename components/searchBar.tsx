318fa00776a4a3831fad9ab8ffa4dddb6b038176
UmerFarooqJillani <umerfarooqjillani92@gmail.com>
2025-12-19 18:26:59 +0500

    Fetch and display Movies on Home Screen

--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -8,8 +8,8 @@ interface props {
 
 const SearchBar = ({ placeholder, onPress }: props) => {
     return (
-        <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
-            <Image source={icons.search} className="size-2"
+        <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-2">
+            <Image source={icons.search} className="size-6"
                 resizeMode="contain" tintColor="#ab8bff" />
             <TextInput
                 onPress={onPress}
